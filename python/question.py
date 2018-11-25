2356b303e1b2db5471d4233f456a1c000a78e77c
superwhiskers <whiskerdev@protonmail.com>
2018-11-24 18:21:54 -0600

    add python implementation

---
+++ python/question.py
@@ -0,0 +1,21 @@
+# an implementation of the question function in python
+def question(prompt, valid):
+	while True:
+		inp = ''
+	
+		print(prompt)
+		if len(valid) != 0:
+			inp = input(f'({", ".join(valid)}): ')
+		else:
+			inp = input(': ')
+		
+		if len(valid) == 0:
+			return inp
+		
+		for ele in valid:
+			if ele == inp:
+				return inp
+		
+		print(f'{inp} is not a valid answer')
+		
+question('foo', ['bar', 'baz'])
