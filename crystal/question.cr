4753c355f8428b579480ecdf36be827892e15f84
superwhiskers <whiskerdev@protonmail.com>
2019-02-19 23:38:34 -0600

    fixes for the crystal question impl - @brecert

--- crystal/question.cr
+++ crystal/question.cr
@@ -1,43 +1,28 @@
 # implementation of the question function in crystal
 def question(prompt : String, valid : Array)
-  
+
   while true
 
-    puts(prompt)
-    if valid.size != 0
+    puts  prompt
+    print "(#{valid.join(", ")})" if valid.size != 0
+    print ": "
 
-      print("(#{valid.join(", ")}): ")
-      
+    input = STDIN.gets
+    
+    if valid.empty?
+
+      return input
+    
     else
 
-      print(": ")
-      
-    end
-
-    inp = STDIN.gets()
-
-    if valid.size == 0
-
-      return inp
-
-    end
-
-    i = 0
-    while i < valid.size()
-
-      if valid[i] == inp
-
-        return inp
-
-      end
-      i += 1
-
-    end
-
-    puts("\"#{inp}\" is not a valid answer")
+      return input if valid.includes? input
     
+    end
+
+    puts %("#{input}" is not a valid answer)  
+
   end
-  
+
 end
 
 question("foo", ["bar", "baz"])
