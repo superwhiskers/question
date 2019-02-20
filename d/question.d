94dc29a34564c09145ef7edf2fe560e7bc8211af
superwhiskers <whiskerdev@protonmail.com>
2019-02-19 22:53:42 -0600

    add d question

---
+++ d/question.d
@@ -0,0 +1,48 @@
+import std.stdio, std.array;
+
+string question(string prompt, string[] valid) {
+
+  string inp;
+
+  while (true) {
+
+    writef("%s\n", prompt);
+    if (valid.length != 0) {
+
+      writef("(%s): ", join(valid, ", "));
+
+    } else {
+
+      writef(": ");
+
+    }
+
+    inp = stdin.readln()[0 .. $-1];
+
+    if (valid.length == 0) {
+
+      return inp;
+
+    }
+
+    foreach (ele; valid) {
+
+      if (ele == inp) {
+
+	return inp;
+
+      }
+
+    }
+
+    writef("\"%s\" is not a valid answer\n", inp);
+
+  }
+  
+}
+
+void main() {
+
+  question("foo", ["bar", "baz"]);
+
+}
