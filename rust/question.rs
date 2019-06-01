df3f8a1403d099483370410e481a21b83795aab8
superwhiskers <whiskerdev@protonmail.com>
2019-05-31 19:04:11 -0500

    add perl implementation

--- rust/question.rs
+++ rust/question.rs
@@ -1,54 +1,38 @@
 use std::io;
-use std::io::{Write};
+use std::io::Write;
 
 // implementation of the question function in rust
 fn question(prompt: &str, valid: Option<&[&str]>) -> String {
-
     let mut input = String::new();
-    
-    loop {
 
+    loop {
         println!("{}", prompt);
         if let Some(valid) = valid {
-
             print!("({}): ", valid.join(", "));
-
         } else {
-
             print!(": ")
-
         }
 
-        io::stdout().flush()
-	    .expect("failed to flush stdout");
-        io::stdin().read_line(&mut input)
+        io::stdout().flush().expect("failed to flush stdout");
+        io::stdin()
+            .read_line(&mut input)
             .expect("failed to read stdin");
         input.pop();
 
         if valid.is_none() {
-
             return input;
-
         }
 
         for ele in valid.unwrap().iter() {
-
             if ele.to_string() == input {
-
-                    return input;
-
+                return input;
             }
-
         }
 
         println!("\"{}\" is not a valid answer", input)
-
     }
-
 }
 
-fn main()  {
-
-  question("foo", Some(&["bar", "baz"]));
-	
+fn main() {
+    question("foo", Some(&["bar", "baz"]));
 }
