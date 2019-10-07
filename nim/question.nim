3144f7815a25b6074165aa9877b9ceca8b61a25b
brecert <11599528+Brecert@users.noreply.github.com>
2019-10-07 12:19:30 -0400

    add nim question

---
+++ nim/question.nim
@@ -0,0 +1,20 @@
+from strutils import join
+
+# implementation of the question "function" in nim
+proc question[I](prompt: string, valid: array[I, string] = []): string =
+  while true:
+    echo prompt
+
+    if valid.len != 0:
+      stdout.write "(", valid.join(", "), ")"
+
+    stdout.write ":"
+
+    var input = stdin.readline
+
+    if input in valid or valid.len == 0:
+      return input
+    else:
+      echo '"', input, '"', " is not a valid answer"
+
+discard question("foo", ["bar", "baz"])
