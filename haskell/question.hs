8ee2397b673a541ce4646d0336ed71f4cdc3ddb6
superwhiskers <whiskerdev@protonmail.com>
2020-04-08 21:46:09 -0500

    split the two possible codepaths into two separate definitions

--- haskell/question.hs
+++ haskell/question.hs
@@ -2,14 +2,17 @@ import System.IO (stdout, hFlush)
 
 -- | implementation of the question function in haskell
 question :: String -> [String] -> IO String
+question prompt [] = do
+    putStrLn prompt
+    putStr ": "
+    hFlush stdout
+    getLine
 question prompt valid = do
     putStrLn prompt
-    if length valid /= 0 then
-        putStr $ "(" ++ ((map (\x -> if x /= last valid then x ++ ", " else x) valid) >>= id) ++ "): "
-    else putStr ": "
+    putStr $ "(" ++ ((map (\x -> if x /= last valid then x ++ ", " else x) valid) >>= id) ++ "): "
     hFlush stdout
     input <- getLine
-    if length valid == 0 || input `elem` valid then return input
+    if input `elem` valid then return input
     else do
         putStrLn $ "\"" ++ input ++ "\" is not a valid answer!"
         question prompt valid
