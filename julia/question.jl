98ea7f0f7779a497df261e226762ebef2ea8355d
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2020-11-11 14:27:09 +0100

    Add Julia

---
+++ julia/question.jl
@@ -0,0 +1,11 @@
+function question(prompt::AbstractString, valid::AbstractVector{<:AbstractString})
+    loopprompt = isempty(valid) ? "" : '(' * join(valid, ", ") * ')'
+    while true
+        print(prompt, '\n', loopprompt, ": ")
+        inp = readline(stdin)
+        (isempty(valid) || inp in valid) && return inp
+        println("\"$inp\" is not a valid answer", '\n')
+    end
+end
+
+question("foo", ["bar", "baz"])
