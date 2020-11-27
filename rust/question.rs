fa76c5a2ffdb6a27a86ecd1dd5dcd365222761ee
superwhiskers <whiskerdev@protonmail.com>
2020-11-27 15:33:35 -0600

    rust: update to match as proposed in #22

--- rust/question.rs
+++ rust/question.rs
@@ -1,42 +1,45 @@
-use std::io;
-use std::io::Write;
+use std::io::{self, stdout, stdin, Write};
 
-// implementation of the question function in rust
-fn question(prompt: &str, valid: Option<&[&str]>) -> String {
+fn question(prompt: &str, valid: Option<&[&str]>) -> io::Result<String> {
+    // declare both the input buffer as well as the string listing the possible responses
+    // beforehand to avoid allocating memory
+    let joined_valid = valid.map(|v| v.join(", "));
     let mut input = String::new();
-    let mut joined_valid: String = "".into();
-    if let Some(valid) = valid {
-        joined_valid = valid.join(", ");
-    }
 
     loop {
+        // output the prompt, displaying possible responses if necessary
         println!("{}", prompt);
-        if valid.is_some() {
-            print!("({})", &joined_valid);
-        }
-        print!(": ");
-
-        io::stdout().flush().expect("failed to flush stdout");
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read stdin");
-        input.pop();
-
-        if valid.is_none() {
-            return input;
+        match &joined_valid {
+            Some(joined_valid) => print!("({}): ", joined_valid),
+            None => print!(": "),
         }
 
-        for ele in valid.unwrap().iter() {
-            if ele.to_string() == input {
-                return input;
-            }
+        // flush the output stream to ensure that the prompt is visible while we await input
+        stdout().flush()?;
+        stdin().read_line(&mut input)?;
+        
+        // trim any line ending from the string and save the resulting slice, not allocating any
+        // new String as we may not need it
+        let trimmed_input = input.trim_end();
+
+        // check if we are accepting all input or if their response is one of the accepted ones
+        if valid.map_or(true, |v| v.contains(&trimmed_input)) {
+            break Ok(trimmed_input.to_string());
         }
 
-        println!("\"{}\" is not a valid answer", input);
-        input = "".to_string();
+        // otherwise, their response was invalid, so we let them know
+        println!("\"{}\" is not a valid answer", trimmed_input);
+
+        // clear the input buffer
+        input.clear();
+
+        // ...and loop around, trying again
     }
 }
 
-fn main() {
-    question("foo", Some(&["bar", "baz"]));
+fn main() -> io::Result<()> {
+    // we may use a better example here instead of a generic one
+    question("foo", Some(&["bar", "baz"]))?;
+    question("foo", None)?;
+    Ok(())
 }
