8c9f82ca5e52d0cf0a9caded5eb3033e00c4f51f
Jonathan <vanillajonathan@users.noreply.github.com>
2021-01-19 10:24:23 +0100

    Format code

--- ruby/question.rb
+++ ruby/question.rb
@@ -1,28 +1,19 @@
-# implementation of the question function in ruby
+# Implementation of the question function in Ruby
 def question(prompt, valid)
-
   while true
-
     puts  prompt
     print "(#{valid.join(", ")})" if valid.size != 0
     print ": "
-
     input = STDIN.gets.chomp
-    
+
     if valid.empty?
-
       return input
-    
     else
-
       return input if valid.include? input
-    
     end
 
     puts %("#{input}" is not a valid answer)  
-
   end
-
 end
 
 question("foo", ["bar", "baz"])
