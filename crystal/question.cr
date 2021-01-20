8bbf1a20f983fb8c9a47fa686607b5857e6586da
brecert <11599528+Brecert@users.noreply.github.com>
2021-01-20 05:50:16 -0500

    run `crystal tool format` on question.cr

--- crystal/question.cr
+++ crystal/question.cr
@@ -1,28 +1,20 @@
 # implementation of the question function in crystal
 def question(prompt : String, valid : Array)
-
   while true
-
-    puts  prompt
+    puts prompt
     print "(#{valid.join(", ")})" if valid.size != 0
     print ": "
 
     input = STDIN.gets
-    
+
     if valid.empty?
-
       return input
-    
     else
-
       return input if valid.includes? input
-    
     end
 
-    puts %("#{input}" is not a valid answer)  
-
+    puts %("#{input}" is not a valid answer)
   end
-
 end
 
 question("foo", ["bar", "baz"])
