53a55b98f4fcedaeed4892d2b6c1a8e2fa593292
bree <me@bree.dev>
2022-02-18 16:45:28 -0500

    powershell: init

---
+++ powershell/question.ps1
@@ -0,0 +1,14 @@
+# implementation of the question function in powershell
+function Question ([Parameter(Mandatory)][string]$Prompt, [string[]]$Valid) {
+  $Prompt = !$Valid.Length ? $Prompt : "$Prompt($($Valid -join ", "))"
+
+  while ($true) {
+    $input = Read-Host($Prompt).Trim()
+    if((!$Valid.Length) -or ($input -in $Valid)) {
+      return $input
+    }
+    Write-Host "`"$input`" is not a valid answer"
+  }
+}
+
+Question foo bar, baz
