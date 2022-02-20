de5b5b16aa8418793c49f873059268bb72f3e6d8
bree <me@bree.dev>
2022-02-20 14:18:03 -0500

    nodejs: update to `v17.5.0` and cleanup code

---
+++ js/node/question.js
@@ -0,0 +1,21 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+// implementation of the question function in nodejs
+async function question(prompt, valid) {
+  const rl = readline.createInterface({ input, output });
+  const joinedPrompt = valid
+    ? `${prompt}(${valid.join(", ")}): `
+    : `${prompt}: `;
+
+  while (true) {
+    const input = await rl.question(joinedPrompt).then((s) => s.trim());
+    if (!valid || valid.includes(input)) {
+      rl.close();
+      return input;
+    }
+    console.log(`"${input}" is not a valid answer`);
+  }
+}
+
+question("foo", ["bar", "baz"]);
