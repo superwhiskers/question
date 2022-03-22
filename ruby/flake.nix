16afdd646e39496144eee387c399a43c19b0f236
bree <me@bree.dev>
2022-03-22 15:51:44 -0400

    ruby: improve flake

--- ruby/flake.nix
+++ ruby/flake.nix
@@ -9,31 +9,32 @@
       inherit self inputs;
 
       outputsBuilder = channels:
-        let pkgs = channels.nixpkgs; in
-        {
-          defaultApp =
-            let
-              name = "question-ruby";
-            in
-            pkgs.stdenv.mkDerivation {
+        let
+          name = "question-ruby";
+          pkgs = channels.nixpkgs;
+        in
+        rec {
+          app.${name} = pkgs.stdenv.mkDerivation {
+            name = name;
+
+            src = builtins.path {
+              path = ./.;
               name = name;
-
-              src = builtins.path {
-                path = ./.;
-                name = name;
-              };
-
-              buildInputs = [
-                pkgs.ruby
-                pkgs.makeWrapper
-              ];
-
-              installPhase = ''
-                mkdir -p $out/bin
-                cp ./question.rb $out/bin/${name}.rb
-                makeWrapper ${pkgs.ruby}/bin/ruby $out/bin/${name} --add-flags $out/bin/${name}.rb
-              '';
             };
+
+            buildInputs = [
+              pkgs.ruby
+              pkgs.makeWrapper
+            ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ./question.rb $out/bin/${name}.rb
+              makeWrapper ${pkgs.ruby}/bin/ruby $out/bin/${name} --add-flags $out/bin/${name}.rb
+            '';
+          };
+
+          defaultApp = app.${name};
 
           devShell = pkgs.mkShell {
             name = "devshell";
