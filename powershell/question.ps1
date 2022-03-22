995d48678fad8b8d89cabd7b9a8d76283b0e2864
bree <me@bree.dev>
2022-03-21 23:55:15 -0400

    powershell: fix newline after prompt

--- powershell/question.ps1
+++ powershell/question.ps1
@@ -1,6 +1,6 @@
 # implementation of the question function in powershell
 function Question ([Parameter(Mandatory)][string]$Prompt, [string[]]$Valid) {
-  $Prompt = !$Valid.Length ? $Prompt : "$Prompt($($Valid -join ", "))"
+  $Prompt = !$Valid.Length ? $Prompt : "$Prompt`n($($Valid -join ", "))"
 
   while ($true) {
     $input = (Read-Host($Prompt)).Trim()
