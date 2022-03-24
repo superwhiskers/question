fbfb16eeb176afb457fb8e5ea7de9eca50558128
bree <me@bree.dev>
2022-03-24 15:27:11 -0400

    nim: strip input and improve implementation

--- nim/question.nim
+++ nim/question.nim
@@ -1,21 +1,18 @@
-from strutils import join
+import std/[strutils, strbasics, strformat, rdstdin]
 
 # implementation of the question "function" in nim
-proc question[I](prompt: string, valid: array[I, string] = []): string =
-  joined_valid = valid.join(", ")
+proc question(prompt: string): string =
+  echo prompt
+  readLineFromStdin(": ")
+
+proc question[I](prompt: string, valid: array[I, string]): string =
+  let joined_prompt = &"""({valid.join(", ")}): """
 
   while true:
     echo prompt
-
-    if valid.len != 0:
-      stdout.write "(", joined_valid, ")"
-    stdout.write ":"
-
-    var input = stdin.readline
-    if input in valid or valid.len == 0:
-      return input
-    else:
-      echo '"', input, '"', " is not a valid answer"
-      continue
+    var input = readLineFromStdin(joined_prompt)
+    input.strip()
+    if input in valid: return input
+    echo '"', input, '"', " is not a valid answer"
 
 discard question("foo", ["bar", "baz"])
