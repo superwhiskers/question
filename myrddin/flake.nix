4586cf55d1fd49bf8ca0b647ae6761525ef42d88
bree <me@bree.dev>
2022-03-25 16:38:43 -0400

    myrddin: add flake

---
+++ myrddin/flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    utils.url = github:gytis-ivaskevicius/flake-utils-plus;
+  };
+
+  outputs = inputs@{ self, nixpkgs, utils }:
+    let
+      name = "question-myrddin";
+    in
+    utils.lib.mkFlake {
+      inherit self inputs;
+
+      outputsBuilder = channels: rec {
+        packages = {
+          ${name} = channels.nixpkgs.stdenv.mkDerivation {
+            name = name;
+
+            src = builtins.path {
+              path = ./.;
+              name = name;
+            };
+
+            nativeBuildInputs = [
+              channels.nixpkgs.myrddin
+            ];
+
+            buildPhase = ''
+              runHook preBuild
+
+              mbld -b question question.myr
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              
+              mkdir -p $out/bin
+              cp ./question $out/bin/${name}
+
+              runHook postInstall
+            '';
+          };
+        };
+
+        defaultPackage = packages.${name};
+        defaltApp = packages.${name};
+
+        devShell = channels.nixpkgs.mkShell {
+          name = "devshell";
+          nativeBuildInputs = [ channels.nixpkgs.myrddin ];
+        };
+      };
+    };
+}
