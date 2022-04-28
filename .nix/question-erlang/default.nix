52e224d4a359cf524b4cd84e88e9912333daccbe
bree <me@bree.dev>
2022-04-28 16:00:47 -0400

    nix: unify erlang

---
+++ .nix/question-erlang/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, stdenv, erlang, makeWrapper, ... }:
+stdenv.mkDerivation rec {
+  name = "question-erlang";
+
+  src = builtins.path {
+    path = ../../erlang;
+    name = name;
+  };
+
+  buildInputs = [ erlang makeWrapper ];
+
+  buildPhase = ''
+    erlc question.erl
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./question.beam $out/bin/${name}.beam
+    makeWrapper ${pkgs.erlang}/bin/escript $out/bin/${name} --add-flags $out/bin/${name}.beam
+  '';
+}
