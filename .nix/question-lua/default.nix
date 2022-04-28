c0804d9220ffd3eb70d785428835aabbb489a424
bree <me@bree.dev>
2022-04-28 16:49:28 -0400

    nix: unify lua

---
+++ .nix/question-lua/default.nix
@@ -0,0 +1,31 @@
+{ self
+, pkgs
+, stdenv
+, lua
+, makeWrapper
+, ...
+}:
+
+stdenv.mkDerivation rec {
+  name = "question-lua";
+
+  src = builtins.path {
+    path = ../../lua;
+    name = name;
+  };
+
+  buildInputs = [
+    lua
+    makeWrapper
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp ./question.lua $out/bin/$name.lua
+    makeWrapper ${pkgs.lua}/bin/lua $out/bin/$name --add-flags $out/bin/$name.lua
+
+    runHook postInstall
+  '';
+}
