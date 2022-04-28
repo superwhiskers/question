c8eb98f04045d9e3449fd38287673c62659433d4
bree <me@bree.dev>
2022-04-28 18:13:37 -0400

    nix: unify python

---
+++ .nix/question-python/default.nix
@@ -0,0 +1,26 @@
+{ self
+, stdenv
+, python3
+, makeWrapper
+, ...
+}:
+
+stdenv.mkDerivation {
+  name = "question-python";
+
+  src = builtins.path {
+    path = ../../python;
+    name = name;
+  };
+
+  buildInputs = [
+    python3
+    makeWrapper
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mv ./question.py $out/bin/$name.py
+    makeWrapper ${python3}/bin/python $out/bin/$name --add-flags $out/bin/$name.py
+  '';
+}
