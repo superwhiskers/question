e94151c9903c7713f655416cdda7014068f97126
bree <me@bree.dev>
2022-04-28 21:31:27 -0400

    nix: rename `uxntal` to `uxn` and unify uxn

---
+++ .nix/question-uxn/default.nix
@@ -0,0 +1,40 @@
+{ self
+, stdenv
+, uxn
+, makeWrapper
+, ...
+}:
+
+
+          stdenv.mkDerivation rec {
+            name = "question-uxn";
+
+            src = builtins.path {
+              path = ../../uxn;
+              name = name;
+            };
+
+            nativeBuildInputs = [
+              uxn
+             makeWrapper
+            ];
+
+            buildPhase = ''
+              runHook preBuild
+
+              uxnasm main.tal question.rom
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+                
+              mkdir -p $out/share/uxn
+              cp ./question.rom $out/share/uxn/$name.rom
+              makeWrapper ${uxn}/bin/uxncli $out/bin/$name --add-flags $out/share/uxn/$name.rom
+
+              runHook postInstall
+            '';
+          }
+        
--- flake.nix
+++ flake.nix
@@ -26,27 +26,60 @@
         rustPlatform = makeRustPlatform {
           inherit (rust.minimal) cargo rustc;
         };
-        
-        importFlakeOutputs = name: path: pkgs: {
-          packages.${name} = callPackage path pkgs;
-          shells.${name} = callPackage (path + "/shell.nix") pkgs;
-        };
 
-        flattenAttrList = lib.lists.foldr (a: b: lib.recursiveUpdate a b) { };
-
-        packages = {
+        mkPackages = pkgs: {
           oak = callPackage ./.nix/oak pkgs;
           dyon = callPackage ./.nix/dyon { inherit pkgs rustPlatform; };
           oakc = callPackage ./.nix/oakc { inherit pkgs rustPlatform; };
           sidef = callPackage ./.nix/sidef pkgs;
         };
 
-        overlay = final: prev: packages;
+        overlay = final: prev: ({
+          # update uxn
+          uxn = prev.uxn.overrideAttrs (old: {
+            version = "85a6d348ba93186eaa328f67c625fea1bacae1f4";
+            src = prev.fetchFromSourcehut {
+              owner = "~rabbits";
+              repo = old.pname;
+              rev = "85a6d348ba93186eaa328f67c625fea1bacae1f4";
+              hash = "sha256-6xd3bvNIaaXNwKpN9+Lolg9qgiSqBjTi6nWyPuHaa3c=";
+            };
+
+            buildPhase = ''
+              runHook preBuild
+
+              ./build.sh --no-run
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              ls
+              install -d $out/bin/ $out/share/${old.pname}/
+              cp ./bin/uxnasm ./bin/uxncli ./bin/uxnemu $out/bin/
+              cp -r projects $out/share/${old.pname}/
+
+              runHook postInstall
+            '';
+          });
+        } // mkPackages prev);
+
+        importFlakeOutputs = name: path: pkgs: {
+          packages.${name} = callPackage path pkgs;
+          shells.${name} = callPackage (path + "/shell.nix") pkgs;
+        };
+
+        flattenAttrList = lib.lists.foldr (a: b: lib.recursiveUpdate a b) { };
       in
       flattenAttrList (lib.lists.flatten [
         {
-          inherit packages;
-          lib = { inherit importFlakeOutputs; };
+          packages = mkPackages pkgs;
+
+          lib = {
+            inherit importFlakeOutputs flattenAttrList;
+          };
         }
 
         # importFlakeOutputs on any dirs starting with "question" in .nix/
